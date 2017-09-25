7b5ac1c75f23537b7e9f53a65de047a473e0adc9
Sergey Pevnev <larockarocka@hotmail.com>
2017-09-25 10:46:13 +0500

    Make infinni.NodeWorker package release

--- .files/Packaging/BuildNupkg.ps1
+++ .files/Packaging/BuildNupkg.ps1
@@ -4,23 +4,23 @@
 #>
 param
 (
-	[Parameter(HelpMessage = "Path to the solution directory.")]
-	[String] $solutionDir = '.',
+  [Parameter(HelpMessage = "Path to the solution directory.")]
+  [String] $solutionDir = '.',
 
-	[Parameter(HelpMessage = "Path to the solution output directory.")]
-	[String] $outputDir = 'Assemblies',
+  [Parameter(HelpMessage = "Path to the solution output directory.")]
+  [String] $outputDir = 'Assemblies',
 
-	[Parameter(HelpMessage = "Path to GlobalAssemblyInfo.cs.")]
-	[String] $assemblyInfo = '.files\Packaging\GlobalAssemblyInfo.cs',
+  [Parameter(HelpMessage = "Path to GlobalAssemblyInfo.cs.")]
+  [String] $assemblyInfo = '.files\Packaging\GlobalAssemblyInfo.cs',
 
-	[Parameter(HelpMessage = "VCS branch name.")]
-	[String] $branchName = '',
+  [Parameter(HelpMessage = "VCS branch name.")]
+  [String] $branchName = '',
 
-	[Parameter(HelpMessage = "VCS commit hash.")]
-	[String] $commitHash = '',
+  [Parameter(HelpMessage = "VCS commit hash.")]
+  [String] $commitHash = '',
 
-	[Parameter(HelpMessage = "Build mode.")]
-	[String] $buildMode = 'Release'
+  [Parameter(HelpMessage = "Build mode.")]
+  [String] $buildMode = 'Release'
 )
 
 # Script dependencies
@@ -35,15 +35,13 @@ Remove-Item -Path $outputDir -Recurse -ErrorAction SilentlyContinue
 $nugetDir = Join-Path $env:ProgramData 'NuGet'
 $nugetPath = Join-Path $nugetDir 'nuget.exe'
 
-if (-not (Test-Path $nugetPath))
-{
-	if (-not (Test-Path $nugetDir))
-	{
-		New-Item $nugetDir -ItemType Directory -ErrorAction SilentlyContinue
-	}
+if (-not (Test-Path $nugetPath)) {
+  if (-not (Test-Path $nugetDir)) {
+    New-Item $nugetDir -ItemType Directory -ErrorAction SilentlyContinue
+  }
 
-	$nugetSourceUri = 'http://dist.nuget.org/win-x86-commandline/latest/nuget.exe'
-	Invoke-WebRequest -Uri $nugetSourceUri -OutFile $nugetPath
+  $nugetSourceUri = 'http://dist.nuget.org/win-x86-commandline/latest/nuget.exe'
+  Invoke-WebRequest -Uri $nugetSourceUri -OutFile $nugetPath
 }
 
 # Restore packages
@@ -55,18 +53,18 @@ if (-not (Test-Path $nugetPath))
 
 # Create nuspec-files
 Create-Nuspec `
-	-solutionDir $solutionDir `
-	-outputDir $outputDir `
-	-assemblyInfo $assemblyInfo `
-	-branchName $branchName `
-	-commitHash $commitHash
+  -solutionDir $solutionDir `
+  -outputDir $outputDir `
+  -assemblyInfo $assemblyInfo `
+  -branchName $branchName `
+  -commitHash $commitHash
 
 Get-ChildItem $outputDir -Filter '*.nuspec' | Foreach-Object {
-	$nuspecFile = Join-Path $outputDir $_.Name
+  $nuspecFile = Join-Path $outputDir $_.Name
 
-	# Create nupkg-file
-	& "$nugetPath" pack $nuspecFile -OutputDirectory $outputDir -NoDefaultExcludes -NonInteractive
+  # Create nupkg-file
+  & "$nugetPath" pack $nuspecFile -OutputDirectory $outputDir -NoDefaultExcludes -NonInteractive
 
-	# Remove nuspec-file
-	Remove-Item -Path $nuspecFile
+  # Remove nuspec-file
+  #Remove-Item -Path $nuspecFile
 }
